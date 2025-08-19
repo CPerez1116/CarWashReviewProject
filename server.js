fcbf7526f6e49beb2abe0cd1edc921ef174bce09
CPerez1116 <cperez@mistercarwash.com>
2025-08-18 22:56:06 -0500

    connected server with index/added post/get routes using express

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require("path"); // Node module to handle file paths
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(express.json());
+
+let reviewsContainer = [];
+
 //serves static files from client folder
 app.use(express.static(path.join(__dirname, "client")));
 
@@ -12,7 +17,26 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
+// GET route used to retrieve reviews in Array
+app.get("/reviews", (req, res) => {
+  res.json(reviewsContainer);
+});
+
+// POST route for storing form submission info
+app.post("/submit-review", (req, res) => {
+  // extracting data from requested body by destructuring
+  const { name, email, review } = req.body;
+
+  // basic validation
+  if (!name || !email || !review) {
+    // status 400 Bad request is standard HTTP meaning the client sent something bad
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  // adding object to array container and also adding timestamp
+  reviewsContainer.push({ name, email, review, timestamp: Date.now() });
+  res.json({ message: "Review submitted!", review: { name, email, review } });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}!`);
 });
-// Local storage so far
